d41e38877cd0c3990aee222576f3c3e32e37ff0b
ChandrakantPal <sanjay161097@gmail.com>
2021-05-15 23:34:49 +0530

    header with logo

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <header>
+      <Image src="https://links.papareact.com/ua6" height={100} width={200} />
+    </header>
+  )
+}
+
+export default Header

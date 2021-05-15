edb0a44a46a18862753ff7c72e401c2b533acbd2
ChandrakantPal <sanjay161097@gmail.com>
2021-05-16 00:25:46 +0530

    header with header items and logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,31 @@
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 
 const Header = () => {
   return (
-    <header>
-      <Image src="https://links.papareact.com/ua6" height={100} width={200} />
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        height={100}
+        width={200}
+      />
     </header>
   )
 }

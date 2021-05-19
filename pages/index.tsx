a14939ad5da9d8d7367f15ba645c831c0f760e86
ChandrakantPal <sanjay161097@gmail.com>
2021-05-20 00:53:01 +0530

    request to tmdb server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/request'
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -12,6 +14,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json())
+
+  return {
+    props: {
+      results: request.results,
+    },
+  }
 }

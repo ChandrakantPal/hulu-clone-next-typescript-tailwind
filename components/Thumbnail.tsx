3e325c565a2f635b30095594399c24fcb06ce72b
ChandrakantPal <sanjay161097@gmail.com>
2021-05-20 02:15:47 +0530

    forwed ref ts issue fixed

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,31 +1,40 @@
 import { ThumbUpIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
-const Thumbnail = ({ result }) => {
+import { forwardRef } from 'react'
+
+type Props = { children: React.ReactNode; result: any }
+export type Ref = HTMLDivElement
+
+const Thumbnail = forwardRef<Ref, Props>((props, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/'
   return (
-    <div className="p-2 transition duration-200 ease-in transform cursor-pointer group sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 transition duration-200 ease-in transform cursor-pointer group sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
+          `${BASE_URL}${
+            props.result.backdrop_path || props.result.poster_path
+          }` || `${BASE_URL}${props.result.poster_path}`
         }
         height={1080}
         width={1920}
       />
       <div className="p-2">
-        <p className="max-w-md truncate">{result.overview}</p>
+        <p className="max-w-md truncate">{props.result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-          {result.title || result.original_name}
+          {props.result.title || props.result.original_name}
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {result.media_type && `${result.media_type} •`}{' '}
-          {result.release_date || result.first_air_date} •{' '}
-          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+          {props.result.media_type && `${props.result.media_type} •`}{' '}
+          {props.result.release_date || props.result.first_air_date} •{' '}
+          <ThumbUpIcon className="h-5 mx-2" /> {props.result.vote_count}
         </p>
       </div>
     </div>
   )
-}
+})
 
 export default Thumbnail

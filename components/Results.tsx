7db37e73b0e4c037767717d52949b945b20a6c9e
ChandrakantPal <sanjay161097@gmail.com>
2021-05-20 02:15:23 +0530

    react flip move added

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,12 +1,13 @@
+import FlipMove from 'react-flip-move'
 import Thumbnail from './Thumbnail'
 
 const Results = ({ results }) => {
   return (
-    <div className="flex-wrap items-center justify-center px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex">
+    <FlipMove className="flex-wrap items-center justify-center px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   )
 }
 

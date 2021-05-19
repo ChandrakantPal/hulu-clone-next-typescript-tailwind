c0d3e6f0806a1c3c369806fe4af36e8d2a453911
ChandrakantPal <sanjay161097@gmail.com>
2021-05-20 01:51:42 +0530

    results added with grid

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,5 +1,13 @@
+import Thumbnail from './Thumbnail'
+
 const Results = ({ results }) => {
-  return <div></div>
+  return (
+    <div className="flex-wrap items-center justify-center px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  )
 }
 
 export default Results
